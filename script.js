fc605b19fd6dc5a0c8e288567d9233b98eb843f3
Wendy Leung <wendysarahleung@gmail.com>
2014-12-27 11:08:42 -0800

    Added moment.js -appended tweet date -tweet date to fromNow date

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
   var $body = $('body');
-  $body.html('');
 
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
     var $tweet = $('<div></div>');
-    $tweet.append('@' + tweet.user + "<p>" + tweet.message + "</p>");
+    $tweet.append('@' + tweet.user + " - " + moment(tweet.created_at).fromNow() + "<p>" + tweet.message + "</p>");
     $tweet.appendTo($body);
     index -= 1;
   }
 
 });
+
+
+
