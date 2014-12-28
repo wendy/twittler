ec80d05a80a2b12e1f0afdfd74a1ecf4ee71cca9
Wendy Leung <wendysarahleung@gmail.com>
2014-12-27 16:45:47 -0800

    Added button to generate new tweets -added class containter for all tweets

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 $(document).ready(function(){
-  var $body = $('body');
+  var $container = $('.container');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.append("<p class='twtusr'>" + '@' + tweet.user + " - " + 
-      "<span class='date'>" + moment(tweet.created_at).fromNow() + "</span>" + "</p>" + 
-      "<p class='twtmsg'>" + tweet.message + "</p>");
-    $tweet.appendTo($body);
-    index -= 1;
-  }
+  var streamTweets = function(i) {
+    var index = streams.home.length - 1;
+    while(i <= index){
+      var tweet = streams.home[i];
+      var $tweet = $('<div class="twt"></div>');
+      $tweet.append("<p class='twtusr'>" + '@' + tweet.user + " - " + 
+        "<span class='date'>" + moment(tweet.created_at).fromNow() + "</span>" + "</p>" + 
+        "<p class='twtmsg'>" + tweet.message + "</p>");
+      $tweet.prependTo($container);
+      i += 1;
+    }
+  };
+  
+  streamTweets(0);
 
+
+  $('button').click(function() {
+    var tweetShown = $('div').length
+    streamTweets(tweetShown);
+  });
 });
 
 
