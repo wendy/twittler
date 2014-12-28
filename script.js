8737ac552e462aa0b07c3575d6585902e0f4ad9c
Wendy Leung <wendysarahleung@gmail.com>
2014-12-27 18:40:13 -0800

    Added hover highlight -resize refresh button

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
     while(i <= index){
       var tweet = streams.home[i];
       var $tweet = $('<div class="twt"></div>');
-      $tweet.append("<p class='twtusr'>" + '@' + tweet.user + " - " + 
-        "<span class='date'>" + moment(tweet.created_at).fromNow() + "</span>" + "</p>" + 
+      $tweet.append("<span class='twtusr'>" + i + '  @' + tweet.user + "</span>" +
+        "<span class='date'>" + " - " + moment(tweet.created_at).fromNow() + "</span>"  + 
         "<p class='twtmsg'>" + tweet.message + "</p>");
       $tweet.prependTo($container);
       i += 1;
@@ -16,10 +16,24 @@ $(document).ready(function(){
   
   streamTweets(0);
 
+  //Refresh Button - appends new tweets to $container
+  $('.refresh').click(function() {
+    var tweetsShown = $('.twt').length;
+    streamTweets(tweetsShown);
+  });
 
-  $('button').click(function() {
-    var tweetShown = $('div').length
-    streamTweets(tweetShown);
+  //Highlight hovered tweet
+  $('.twt').mouseenter(function() {
+    $(this).addClass('hoveredtwt');
+  });
+
+  $('.twt').mouseleave(function() {
+    $(this).removeClass('hoveredtwt');
+  });
+  
+  //Click User Name for more tweets from thats user
+  $('.twtusr').click(function() {
+
   });
 });
 
